389f0cde436c9e6fddf98eb8320668f258debc23
Bernardo hazin <bernardo.hazin@gmail.com>
2019-05-12 14:26:22 -0300

    removendo apollo-server, alterando paths

--- server.js
+++ server.js
@@ -1,29 +1,5 @@
 const express = require('express')
 const path = require('path')
-const server = require('vue-cli-plugin-apollo/graphql-server')
-
-const opts = {
-  host: 'localhost',
-  port: 4000,
-  graphqlPath: '/graphql',
-  subscriptionsPath: '/graphql',
-  enableMocks: false,
-  enableEngine: false,
-  cors: '*',
-  timeout: 1000000,
-  quiet: true,
-  paths: {
-    typeDefs: require.resolve('./apollo-server/type-defs.js'),
-    resolvers: require.resolve('./apollo-server/resolvers.js'),
-    context: require.resolve('./apollo-server/context.js'),
-    directives: require.resolve('./apollo-server/directives.js'),
-    dataSources: require.resolve('./apollo-server/data-sources.js')
-  }
-}
-
-server(opts, () => {
-  console.log('Apollo server is running!')
-})
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'dist')))
